bac0896858bf79694d826b20af54f0d11773312d
shahista <shahista@shahista-Presario-C700-Notebook-PC.(none)>
2014-02-25 12:35:35 +0530

    add menu and tab in item,schoo,fees

---
+++ epan-components/schoolERPApp/page/owner/masterschool.php
@@ -0,0 +1,13 @@
+<?php
+class page_schoolERPApp_page_owner_masterschool extends page_componentBase_page_owner_main{
+	function init(){
+		parent::init();
+	$tab=$this->add('Tabs');
+	$tab->addTabURL('schoolERPApp_page_owner_master_class','Class');
+	$tab->addTabURL('schoolERPApp_page_owner_school_attendence','Attendence');
+	$tab->addTabURL('schoolERPApp_page_owner_student','Student');
+	$tab->addTabURL('schoolERPApp_page_owner_master_schoolar','Schoolar');
+	$tab->addTabURL('schoolERPApp_page_owner_master_section','Section');
+	$tab->addTabURL('schoolERPApp_page_owner_master_subject','Subject');	
+	}
+}
