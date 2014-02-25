4c739102011eaa504e1042c37036cee67e139d75
shahista <shahista@shahista-Presario-C700-Notebook-PC.(none)>
2014-02-25 12:31:15 +0530

    add menu and tabs of school,fees,item

--- epan-components/schoolERPApp/page/owner/main.php
+++ epan-components/schoolERPApp/page/owner/main.php
@@ -3,6 +3,9 @@
 class page_schoolERPApp_page_owner_main extends page_componentBase_page_owner_main{
 function init(){
 	parent::init();
-	
-}
+	$menu=$this->add('Menu',null);
+	$menu->addMenuItem('schoolERPApp_page_owner_masterschool','School');
+	$menu->addMenuItem('schoolERPApp_page_owner_masterfees','Fees');
+	$menu->addMenuItem('schoolERPApp_page_owner_masteritem','item');
+ }
 }
