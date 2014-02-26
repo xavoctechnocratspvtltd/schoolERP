4363414ef45064b902a693e5fd56939814308c64
shahista <shahista@shahista-Presario-C700-Notebook-PC.(none)>
2014-02-26 10:13:29 +0530

    use redirect condition in project

--- epan-components/schoolERPApp/page/owner/student.php
+++ epan-components/schoolERPApp/page/owner/student.php
@@ -3,13 +3,11 @@ class page_schoolERPApp_page_owner_student extends page_componentBase_page_owner
 	function init(){
 		parent::init();
 $this->add('H3')->setHTML('<center>Student Admission Form</center>');
-$col=$this->add('Columns');
-$col1=$col->addColumn(6);
-$form=$col1->add('Form');
-$form->setModel('schoolERPApp/School_Student');
-$form->addSubmit('SAVE');
-if($form->isSubmitted()){
-	$form->js()->reload()->execute();
-}
+$crud=$this->add('CRUD');
+$crud->setModel('schoolERPApp/School_Student');
+
+	// $form->update();
+		
+
 }
 }
