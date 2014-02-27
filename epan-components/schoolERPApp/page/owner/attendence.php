ac7af443429113d1cf7574fd29700b15431eb8fc
shahista <shahista@shahista-Presario-C700-Notebook-PC.(none)>
2014-02-27 14:30:40 +0530

    push code

--- epan-components/schoolERPApp/page/owner/attendence.php
+++ epan-components/schoolERPApp/page/owner/attendence.php
@@ -3,48 +3,53 @@ class page_schoolERPApp_page_owner_attendence extends page_componentBase_page_ow
 	function init(){
 		parent::init();
 
-    $this->add('CRUD')->setModel('schoolERPApp/School_Attendence');
+    // $this->add('CRUD')->setModel('schoolERPApp/School_Attendence');
 		
-// 	$col=$this->add('H3')->setAttr('align','center')->set('Attendence Detail');
-//     $col=$this->add('Columns');
-//     $co1=$col->addColumn(6);
+	$col=$this->add('H3')->setAttr('align','center')->set('Attendence Detail');
+    $col=$this->add('Columns');
+    $co1=$col->addColumn(6);
     
-//     $form=$co1->add('Form');
-//     $class=$form->addField('dropdown','class')->setEmptyText('----');
-//     $col=$this->add('schoolERPApp/Model_Master_Class');
-//     $month=$form->addField('dropdown','month')->setEmptyText('----');
-//     $month->setValueList(array('1'=>'January','2'=>'Feb',));
+    $form=$co1->add('Form');
+    $class_field=$form->addField('dropdown','class')->setEmptyText('----');
+    $class=$this->add('schoolERPApp/Model_Master_Class');
+    $month=$form->addField('dropdown','month')->setEmptyText('----');
+    $month->setValueList(array('1'=>'January','2'=>'Feb',));
 
-//     $att=$form->addField('line','Total_attendence');
-//     $form->addField('checkbox','change_total_attendance');
-//     $form->addSubmit('Go');
- 	
+    $att=$form->addField('line','Total_attendence');
+    $form->addField('checkbox','change_total_attendance');
+    $form->addSubmit('Go');
+ 	  
 
-//     $grid=$this->add('Grid');
+    $grid=$this->add('Grid');
+    $student=$this->add('schoolERPApp/Model_School_Student');
+    // $student->_dsql()->del('order');//->order('name','asc');
+    $grid->setModel($student,array('name','gender'));
+    // $grid->addClass('reloadable');
+    $grid->js()->reload();
+    // $grid->js('reloadme',$grid->js()->reload());
 
-//         $b=$this->add('schoolERPApp/Model_School_Attendence');
-//         if($_GET['attendence_id']){
-//           $b->load($_GET['attendence_id']);
-//         }
 
-//         if($_GET['delete']){
-//           $b->load($_GET['delete'])->delete();
-//           $grid->js()->reload()->execute();
-//         }
-//         if($_GET['edit']){
-//           $form->js()->reload(array('attendence_id'=>$_GET['edit']))->execute();
-//         }
-//     $grid->setModel($col);
-//     $class->setModel($col);
-//     $grid->addColumn('Button','edit');
-//     $grid->addColumn('Confirm','delete');
-//          if($form->isSubmitted()){
-//         $atten=$this->add('schoolERPApp/Model_School_Attendence');
-//         //$atten->addCondition('class_id',$form->get('class'));
-//         $atten->addCondition('month',$form->get('month'));
-//           $form->go();
-//           $form->js(null,$grid->js()->reload())->reload()->execute();
-//          }  
-        
+    if($_GET['class_id'])
+    	$student->addCondition('schoolERPApp_class_id',$_GET['class_id']);
+
+        $b=$this->add('schoolERPApp/Model_School_Attendence');
+        if($_GET['attendence_id']){
+          $b->load($_GET['attendence_id']);
+        }
+
+        if($_GET['delete']){
+          $b->load($_GET['delete'])->delete();
+          $grid->js()->reload()->execute();
+        }
+        if($_GET['edit']){
+          $form->js()->reload(array('attendence_id'=>$_GET['edit']))->execute();
+        }
+    $grid->setModel($student);
+    $class_field->setModel($class);
+    $grid->addColumn('Button','edit');
+    $grid->addColumn('Confirm','delete');
+    if($form->isSubmitted()){
+    $grid->js()->reload(array('class_id'=>$form->get('class')))->execute();
+         }  
 }
 }   
